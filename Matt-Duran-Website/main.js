89b4e3c1bc5c4c16f4b1f386ef771c4f658b07a1
Matthew Duran <mduran2429@gmail.com>
2021-08-06 11:41:23 -0400

    cleaned up code

--- Matt-Duran-Website/main.js
+++ Matt-Duran-Website/main.js
@@ -24,14 +24,14 @@ ready(() => {
 
 
 /* videoPlayer code */
-                var videoPlayer = document.getElementById("videoPlayer");
-                var myVideo = document.getElementById("myVideo");
+var videoPlayer = document.getElementById("videoPlayer");
+var myVideo = document.getElementById("myVideo");
 
-                function stopVideo(){
-                    videoPlayer.style.display = "none"
-                }
+function stopVideo(){
+    videoPlayer.style.display = "none"
+}
 
-                function playVideo(file){
-                    myVideo.src = file;
-                    videoPlayer.style.display = "block"
-                }
+function playVideo(file){
+    myVideo.src = file;
+    videoPlayer.style.display = "block"
+}
